f8559dc1f5a913f3021000314b9067dd2faea99d
Mehar Mutreja <76557895+7373Mehar@users.noreply.github.com>
2022-01-11 07:31:27 -0800

    Understanding Variables

---
+++ Variables.py
@@ -0,0 +1,7 @@
+character_name = "Mehar"
+character_age = "21"
+print("My name is " +character_name + ",")
+print("I am " + character_age + " years old.")
+character_name = "Mike"
+print("I like the name " + character_name + ",")
+print("I dislike being " + character_age + ".")
