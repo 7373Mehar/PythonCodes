1ff11f7f0240c5310fafe9b2d9c28efd5a9d7d04
Mehar Mutreja <76557895+7373Mehar@users.noreply.github.com>
2022-01-12 00:58:46 -0800

    Strings

---
+++ Strings.py
@@ -0,0 +1,41 @@
+#Strings
+print("Mehar Mutreja")
+print("Mehar\nMutreja")
+
+#Using Variables
+Quote = "With Great Power Comes Great Responsibility!"
+quote = "I am the senate!"
+print(quote)
+print(Quote)
+
+#Concatenation
+Quote = "With Great Power Comes Great Responsibility!"
+print(Quote + "I am the senate!")
+
+#Concatenation-2
+_quote= "Anakin is Jedi"
+print(_quote + " in star wars")
+
+#Some Functions
+_quote= "Anakin is Jedi"
+print(_quote.lower())
+print(_quote.upper())
+
+#Using 2 Functions Together
+_quote= "Anakin is Jedi"
+print(_quote.upper().isupper())
+
+#Counting the lenght
+_quote= "Anakin is Jedi"
+print(len(_quote))
+
+#Index
+_quote= "Anakin_is Jedi"
+print(_quote[6])
+print(_quote.index("J"))
+
+#Replace
+_quote= "Anakin is Jedi"
+print(_quote.replace("Anakin","Obi-Wan"))
+
+
