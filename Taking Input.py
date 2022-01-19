ef557ac50d56f4465f0c759c08fc05e9591b655c
Mehar Mutreja <76557895+7373Mehar@users.noreply.github.com>
2022-01-19 05:10:09 -0800

    Taking Input

---
+++ Taking Input.py
@@ -0,0 +1,18 @@
+#Taking Input
+name = input("Enter your name:")
+age = input("Enter your age:")
+print("Hello " + name + "."" You are " + age + " years old.")
+
+#Small Calculator
+num1 = input("Enter a number:")
+num2 = input("Enter a number:")
+result = float(num1) + float(num2)
+print(result)
+
+#Madlibs Game
+fav_player = input("Player Name:")
+jersey_num = input("Enter Jersey Number:")
+position = input("Enter Position:")
+print("My favourite player is " + fav_player + ".")
+print("His jersey number is " + jersey_num + ".")
+print("He plays at a " + position + " position.")
